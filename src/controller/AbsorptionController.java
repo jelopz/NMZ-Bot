71f4a628bfa0a49b848c3d8bcac2299df7febc55
Jesus Lopez <jlopez92@unm.edu>
2017-09-23 21:47:31 -0600

    Initialize AbsorptionController.java

---
+++ src/controller/AbsorptionController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class AbsorptionController {
+	private boolean absorpEmpty;
+
+	public AbsorptionController() {
+		absorpEmpty = false;
+	}
+	
+	public boolean isEmpty() {
+		return absorpEmpty;
+	}
+}
